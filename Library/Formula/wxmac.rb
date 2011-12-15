35690a462a4e57e01589ae526e03ab516337beea
Nicola Tuveri <nic.tuv@gmail.com>
2011-12-15 12:57:45 +0100

    wxWidgets 2.9.3 only

--- Library/Formula/wxmac.rb
+++ Library/Formula/wxmac.rb
@@ -11,24 +11,13 @@ def which_python
 end
 
 class Wxpython < Formula
-  if ARGV.build_devel?
-    url 'http://downloads.sourceforge.net/project/wxpython/wxPython/2.9.2.4/wxPython-src-2.9.2.4.tar.bz2'
-    md5 '8dae829b3bb3ccfe279d5d3948562c5f'
-  else
-    url 'http://downloads.sourceforge.net/wxpython/wxPython-src-2.8.11.0.tar.bz2'
-    md5 '63f73aae49e530852db56a31b57529fa'
-  end
+  url 'http://downloads.sourceforge.net/project/wxpython/wxPython/2.9.2.4/wxPython-src-2.9.2.4.tar.bz2'
+  md5 '8dae829b3bb3ccfe279d5d3948562c5f'
 end
 
 class Wxmac < Formula
-  if ARGV.build_devel?
-    url 'http://downloads.sourceforge.net/project/wxwindows/2.9.2/wxWidgets-2.9.2.tar.bz2'
-    md5 'd6cec5bd331ba90b74c1e2fcb0563620'
-    version '2.8.11'
-  else
-    url 'http://downloads.sourceforge.net/project/wxwindows/2.8.11/wxMac-2.8.11.tar.bz2'
-    md5 '8d84bfdc43838e2d2f75031f62d1864f'
-  end
+  url 'http://downloads.sourceforge.net/project/wxwindows/2.9.3/wxWidgets-2.9.3.tar.bz2'
+  md5 '6b6003713289ea4d3cd9b49c5db5b721'
   homepage 'http://www.wxwidgets.org'
 
   def options
@@ -36,21 +25,6 @@ class Wxmac < Formula
       ['--python', 'Build Python bindings'],
       ['--system-python', 'Build against the OS X Python instead of whatever is in the path.']
     ]
-  end
-
-  def test_python_arch
-    system "arch -i386 #{which_python} --version"
-  rescue
-    onoe "No python on path or default python does not support 32-bit."
-    puts <<-EOS.undent
-      Your default python (if any) does not support 32-bit execution, which is
-      required for the wxmac python bindings. You can install the Homebrew
-      python with 32-bit support by running:
-
-      brew install python --universal --framework
-
-    EOS
-    exit 99
   end
 
   def install_wx_python
@@ -69,45 +43,26 @@ class Wxmac < Formula
       "BUILD_GIZMOS=0"
     ]
     Dir.chdir "wxPython" do
-      system "arch", "-i386",
-                     which_python,
-                     "setup.py",
-                     "build_ext",
-                     *opts
+      system which_python,
+          "setup.py",
+          "build_ext",
+          *opts
 
-      system "arch", "-i386",
-                     which_python,
-                     "setup.py",
-                     "install",
-                     "--prefix=#{prefix}",
-                     *opts
+      system which_python,
+          "setup.py",
+          "install",
+          "--prefix=#{prefix}",
+          *opts
     end
   end
 
   def install
-    test_python_arch if build_python?
-
-    unless ARGV.build_devel?
-      # Force i386
-      %w{ CFLAGS CXXFLAGS LDFLAGS OBJCFLAGS OBJCXXFLAGS }.each do |compiler_flag|
-        ENV.remove compiler_flag, "-arch x86_64"
-        ENV.append compiler_flag, "-arch i386"
-      end
-    end
-
-    if ARGV.build_devel?
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
                             "--prefix=#{prefix}",
                             "--with-osx-cocoa",
                             "--enable-unicode",
-                            "--with-macosx-sdk=/Developer/SDKs/MacOSX10.6.sdk"
-    else
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}",
-                            "--enable-unicode",
                             "--with-macosx-sdk=/Developer/SDKs/MacOSX10.6.sdk",
                             "--with-macosx-version-min=10.6"
-    end
 
     system "make install"
 
@@ -119,27 +74,12 @@ class Wxmac < Formula
 
   def caveats
     s = ""
-    unless ARGV.build_devel?
-      s += <<-EOS.undent
-        wxWidgets 2.8.x builds 32-bit only, so you probably won''t be able to
-        use it for other Homebrew-installed softare on Snow Leopard (like Erlang).
-
-      EOS
-    end
 
     if build_python?
       s += <<-EOS.undent
         Python bindings require that Python be built as a Framework; this is the
         default for Mac OS provided Python but not for Homebrew python (compile
         using the --framework option).
-
-        You will also need 32-bit support for Python. If you are on a 64-bit
-        platform, you will need to run Python in 32-bit mode:
-
-          arch -i386 python [args]
-
-        Homebrew Python does not support this by default (compile using the
-        --universal option)
 
       EOS
     end
